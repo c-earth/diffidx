892f3ea0ad948e4c6491334bc2ad15f6901e86cb
c-earth <c_earth@mit.edu>
2024-08-22 16:08:44 -0400

    restructure

--- utils/model.py
+++ utils/model.py
@@ -1,2 +1,8 @@
-import torch
+import torch.nn as nn
+from savemodel.save import SaveModel
 
+class Test(nn.Module):
+    pass
+
+tracker = SaveModel(Test, None, None, './utils', './models/test', None, None, None, None)
+tracker.save()
